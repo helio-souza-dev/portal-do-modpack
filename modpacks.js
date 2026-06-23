065b768e3be532a915a697695caafa89a4aefd94
Hélio de Souza Costa Alves <heliosoltb@gmail.com>
2026-06-22 23:01:31 -0300

    Update modpacks.js

--- modpacks.js
+++ modpacks.js
@@ -1,7 +1,1 @@
-const MODPACKS_CONFIG = [
-    {
-        nome: "A Era das Máquinas v1.5",
-        codigo: "Exemplo123",
-        link: "https://drive.google.com/..."
-    }
-];
+const MODPACKS_CONFIG = [];
