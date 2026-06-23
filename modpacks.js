08be97e7bb4110b1068e411dbc0d7e4859a78f9b
Hélio de Souza Costa Alves <heliosoltb@gmail.com>
2026-06-22 23:04:26 -0300

    v1.6

--- modpacks.js
+++ modpacks.js
@@ -1,1 +1,7 @@
-const MODPACKS_CONFIG = [];
+const MODPACKS_CONFIG = [
+    {
+        "nome": "Renha (Society Sunlit Valley)",
+        "codigo": "EzXV3woqqi",
+        "link": "https://drive.google.com/drive/folders/1WrJnSgY4QuK2HoH21pO3R0f3cYNhnzi9"
+    }
+];
