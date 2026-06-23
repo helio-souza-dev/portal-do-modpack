5c91edcc125ab96d5cf3ca48b0073bdf8ce87b9b
Hélio de Souza Costa Alves <heliosoltb@gmail.com>
2026-06-22 23:13:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -193,6 +193,12 @@ async function sendToDiscord() {
         inline: false
     });
 
+    embed.fields.push({
+        name: "🔍 **NÃO SABE QUE MOD FALTA?**",
+        value: `> 🔗 **[Use o Comparador de Mods](https://portal-do-modpack-5i3yhj2nt-saragospel30-3223s-projects.vercel.app/)**`,
+        inline: false
+    });
+
     const payload = {
         content: mention || null,
         embeds: [embed]
